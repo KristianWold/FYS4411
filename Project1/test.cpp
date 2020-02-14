f6bc979a22fd78e206b86f7bbb811940f94b7804
KristianWold <kriswold@student.matnat.uio.no>
2020-02-14 18:13:31 +0100

    gtest up and running

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -1,24 +1,17 @@
-#include "../src/system.hpp"
-#include "../src/particles.hpp"
-#include "../src/Hamiltonians/hamiltonian.hpp"
-#include "../src/Hamiltonians/harmonicoscillator.hpp"
-#include "../src/WaveFunctions/wavefunction.hpp"
-#include "../src/WaveFunctions/simplegaussian.hpp"
+#include "src/system.hpp"
+#include "src/particles.hpp"
+#include "src/Hamiltonians/hamiltonian.hpp"
+#include "src/Hamiltonians/harmonicoscillator.hpp"
+#include "src/WaveFunctions/wavefunction.hpp"
+#include "src/WaveFunctions/simplegaussian.hpp"
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
 TEST(SquareRootTest, PositiveNos) {
-    EXPECT_EQ (18.0, square‑root (324.0));
-    EXPECT_EQ (25.4, square‑root (645.16));
-    EXPECT_EQ (50.3321, square‑root (2533.310224));
+    EXPECT_EQ (1, 1);
 }
 
-TEST (SquareRootTest, ZeroAndNegativeNos) {
-    ASSERT_EQ (0.0, square‑root (0.0));
-    ASSERT_EQ (‑1, square‑root (‑22.0));
-}
-
-int main(int argc, char ∗∗argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+/*int main(int argc, char const *argv[]){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}*/
